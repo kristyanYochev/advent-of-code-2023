fbe916b5ddb5e9d2968e586423dee888dcae7d73
Kristiyan Yochev <kris.yochev@gmail.com>
2023-12-01 23:44:41 +0000

    Added test for old behaviors that should carry through

--- day_01/rust/trebuchet/src/bin/part_two.rs
+++ day_01/rust/trebuchet/src/bin/part_two.rs
@@ -1,1 +1,19 @@
 fn main() {}
+
+fn extract_calibration_number(line: &str) -> u32 {
+    todo!();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::extract_calibration_number;
+
+    #[test]
+    fn rules_from_part_1_apply() {
+        assert_eq!(extract_calibration_number("1abc2"), 12);
+        assert_eq!(extract_calibration_number("pqr3stu8vwx"), 38);
+        assert_eq!(extract_calibration_number("a1b2c3d4e5f"), 15);
+        assert_eq!(extract_calibration_number("treb7uchet"), 77);
+        assert_eq!(extract_calibration_number("l1ne"), 11);
+    }
+}
